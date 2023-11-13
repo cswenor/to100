edb3b5e9fc64cf9cb6b0b3711167a5d3186021eb
Chris Swenor <cswenor@gmail.com>
2023-11-13 00:54:49 -0500

    Fixed the game logic

--- src/ai.js
+++ src/ai.js
@@ -4,8 +4,11 @@ function aiTurn(currentScore, callback) {
     let aiChoice;
 
     if (isHardMode) {
-        aiChoice = 11 - (currentScore % 11); 
-        if (aiChoice > 10) aiChoice = 10; 
+        aiChoice = (11 - (currentScore % 11)) % 11 + 1;
+        if (aiChoice === 0 || aiChoice > 10) {
+            // If the modulo operation results in 0 or a number greater than 10, choose 1
+            aiChoice = 1;
+        }  
     } else {
         // Random choice in Easy mode
         aiChoice = Math.floor(Math.random() * 10) + 1;
